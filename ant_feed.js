4e45ecffa1cbe6c001ef39cd8ba265ecdd418010
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-03-26 17:09:17 +0200

    first step

--- ant_feed.js
+++ ant_feed.js
@@ -2,18 +2,50 @@ const Ant = require('aai-ant-plus');
 const stick = new Ant.GarminStick3;
 const cadenceSensor = new Ant.CadenceSensor(stick);
 const speedSensor = new Ant.SpeedSensor(stick);
+const express = require("express")
+const socketIO = require('socket.io');
+const http = require('http')
+
+let server = http.createServer(express()) 
+let cr= {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["content-type"]
+  }
+let io = socketIO(server,{cors:cr}) 
+let socket;
 
 var checkOldval=-1;
 let lastPrintSpeedTime = Date.now();
 let lastPrintCadenceTime = Date.now();
 
+function initSocket(){
+    server.listen(4321)
+    io.on('connection', (sock)=>{
+        socket=sock;
+        console.log(`New user connected ${socket.id}`)
+        socket.on("message", (data) => {
+            let event = JSON.parse(data)
+            console.log('EVENT',event)
+        })
+    });
+}
+
+function send(msg){
+    if(socket){
+        socket.emit('data',msg);
+    }
+        
+}
 async function initANT(){
-    speedSensor.setWheelCircumference(2.118);
+    initSocket();
+    speedSensor.setWheelCircumference(4.818);
     speedSensor.on('speedData', async data => {
         if(checkOldval!=data.CalculatedSpeed){
             const currentTime = Date.now();
             if (currentTime - lastPrintSpeedTime >= 400) { 
               console.log('SPEED  ', new Date(), data.CalculatedSpeed, data.CalculatedDistance);
+              send({ts:new Date(),speed:data.CalculatedSpeed,distance:data.CalculatedDistance})
               lastPrintSpeedTime = currentTime;
             }
             checkOldval=data.CalculatedSpeed;
@@ -25,6 +57,7 @@ async function initANT(){
             const currentTime = Date.now();
             if (currentTime - lastPrintCadenceTime >= 400) { 
                 console.log('CADENCE',new Date(),data.CalculatedCadence);
+                send({ts:new Date(),cadence:data.CalculatedCadence})
                 lastPrintCadenceTime = currentTime;
             }
         }   

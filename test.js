375ba55c4d0432eadabad3430d873968cabc9372
Anthony Alteirac <105443206+sfc-gh-aalteirac@users.noreply.github.com>
2023-03-27 22:34:47 +0200

    with socket

--- test.js
+++ test.js
@@ -1,11 +1,46 @@
 const { SpeedSensor } = require('incyclist-ant-plus');
 const { AntDevice } = require('incyclist-ant-plus/lib/bindings')
+const express = require("express")
+const socketIO = require('socket.io');
+const http = require('http')
+const port = process.env.PORT || 4321;
+
+let server = http.createServer(express()) 
+let cr= {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["content-type"]
+  }
+let io = socketIO(server,{cors:cr}) 
+
+let socket;
 
 const ant = new AntDevice({ startupTimeout: 2000 })
 const msg_limit=1000;
-var lastMsgTS= Date.now();
+let lastPrintSpeedTime = Date.now();
+let lastPrintCadenceTime = Date.now();
 
-async function main(deviceID = -1) {
+function initSocket(){
+    server.listen(port)
+    io.on('connection', (sock)=>{
+        socket=sock;
+        console.log(`New user connected ${socket.id}`)
+        socket.on("message", (data) => {
+            let event = JSON.parse(data)
+            console.log('EVENT',event)
+        })
+    });
+}
+
+function send(msg){
+    if(socket){
+        socket.emit('data',msg);
+    }
+        
+}
+
+async function initAnt(deviceID = -1) {
+    initSocket();
     const opened = await ant.open()
     if (!opened) {
         console.log('could not open Ant Stick')
@@ -30,15 +65,17 @@ async function main(deviceID = -1) {
     else {  // device ID known
         console.log(`Connecting with id=${deviceID}`)
         const speedSensor = new SpeedSensor(deviceID)
+        speedSensor.setWheelCircumference(4.818);
         channel.startSensor(speedSensor)
     }
 }
 
 function onData(profile, deviceID, data) {
     const currentTime=Date.now();
-    if(currentTime-lastMsgTS>=msg_limit){
-        console.log(`id: ANT+${profile} ${deviceID}, speed: ${data.CalculatedSpeed}, distance: ${data.CalculatedDistance}, revol:${data.CumulativeSpeedRevolutionCount},revol:${data.Motion}`);
-        lastMsgTS=currentTime;
+    if(currentTime-lastPrintSpeedTime>=msg_limit){
+        console.log(`id: ANT+${profile} ${deviceID}, speed: ${data.CalculatedSpeed}, distance: ${data.CalculatedDistance}, TotalRevolutions:${data.CumulativeSpeedRevolutionCount},Motion:${data.Motion}`);
+        send({ts:new Date(),speed:data.CalculatedSpeed,distance:data.CalculatedDistance})
+        lastPrintSpeedTime=currentTime;
     }
 
 }
@@ -56,4 +93,4 @@ process.on('SIGTERM', async () => await onAppExit()); // `kill` command
 const args = process.argv.slice(2);
 const deviceID = args.length > 0 ? args[0] : undefined;
 
-main(deviceID);
+initAnt(deviceID);
